e4899542dd7ab2f6f90b6c8b6accdff8b10ea41b
Luis Mejia <luis@appsumo.com>
2022-07-20 12:20:43 -0600

    Fix package name

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,3 +112,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
